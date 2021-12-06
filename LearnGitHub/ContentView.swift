251f50a3f58f27222352f0c39beaf7994bc02529
Luke Burke <lukeburke@gmail.com>
2021-12-06 10:46:47 -0500

    Initial Commit

---
+++ LearnGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnGitHub
+//
+//  Created by Luke Burke on 12/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnGitHub/LearnGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitHubApp.swift
+//  LearnGitHub
+//
+//  Created by Luke Burke on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
